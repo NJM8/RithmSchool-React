082b199dc7c47458b3f2f0536f23a05910b8c946
Nate Mallison <healthynate@gmail.com>
2019-09-23 08:47:43 -0400

    Added some basic examples with props, my MyComponent usage.

--- basic-component/app.jsx
+++ basic-component/app.jsx
@@ -1,3 +1,26 @@
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Here is a component!</h2>
+      </div>
+    )
+  }
+}
+
+class AppTwo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Here is my second React app!</h1>
+        <MyComponent/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<AppTwo />, document.getElementById("app-two"));
+
 class App extends React.Component {
   render() {
     return (
@@ -9,3 +32,15 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
+
+class AppProps extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello { this.props.name }!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<AppProps name="Nate"/>, document.getElementById("app-props"));
